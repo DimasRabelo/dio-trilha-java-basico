01769b1967d71a90938874e391b29304145dcb7b
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-13 15:37:14 -0300

    Arrays

---
+++ Estrututura de Repeticao/src/ExemploForEachArray.java
@@ -0,0 +1,9 @@
+public class ExemploForEachArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+       for (String aluno : alunos) {
+            System.out.println("Nome do Aluno é: " + aluno);
+        }
+    }
+}
