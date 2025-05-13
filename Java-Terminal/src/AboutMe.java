3c7d4f4185c0004cb719d8e5f87ea2df9bdbcd7a
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-12 21:44:15 -0300

    java-terminal

--- Java-Terminal/src/AboutMe.java
+++ Java-Terminal/src/AboutMe.java
@@ -1,5 +1,14 @@
 public class AboutMe {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // os argumentos começam com indice 0
+        String nome = args[0];
+        String sobrenome = args[1];  
+        int idade = Integer.parseInt(args[2]); 
+        double altura = Double.valueOf(args[3]);
+        
+        System.out.println("Ola, Me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha Altura É: " + altura + "cm");
+    
     }
 }
