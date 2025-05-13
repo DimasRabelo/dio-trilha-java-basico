403ffae30d0e2bb14c52ac01ac27d3d4e788369f
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-13 14:41:20 -0300

    Switch-Case

---
+++ Controle-de-Fluxo/src/OperadoraSwitch.java
@@ -0,0 +1,17 @@
+public class OperadoraSwitch {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5Gb Youtube");
+                }
+                case "M":{
+                System.out.println("Whats e Instagran grátis");
+                }
+                case "B":{
+                System.out.println("100 minutos de ligação");
+                }
+        }
+    }
+}
